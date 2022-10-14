97e721b764d8da4d65eac810f9f639f8c75db50b
Mouad Serroukh <mserrouk@e2r12p1.1337.ma>
2022-10-14 17:38:06 +0100

    working in progress

--- ft_memmove.c
+++ ft_memmove.c
@@ -4,18 +4,19 @@
    void *ft_memmove(void *dst, const void *src,size_t len)
    {
      size_t i;
+     i = 0;
      char *str,* dstr,*swap;
 
-        str = (const char*)src;
-        // dstr = (char*) dst;
-        // while(str[i] && i < len)
-        //     i++;
-        i--;
+        str = ( char*)src;
+        dstr = (char*) dst;
+        while(str[i] && i < len)
+            i++;
+        i -= 2;
         len--;
         while((str[i]) )
         {
-            // dstr[i] = str[i];
-            // len--;
+            dstr[i] = str[i];
+            len--;
             i++;
         }
         return 0;
@@ -24,7 +25,7 @@
 {
     char src1[] ="mmm";
     char str[] = "hello";
-    ft_memmove(str,src1,2);
+    memmove(str,src1,2);
     printf("%s\n",str);
     printf("%s\n",src1);
 
---
+++ ft_strlcat.c
@@ -0,0 +1,43 @@
+#include <string.h>
+#include<stdio.h>
+
+size_t ft_strlen(char  *str)
+{
+    size_t  i;
+    i = 0;
+    while((str[i]))
+        i++;
+    return i;    
+}
+
+
+size_t  ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
+{
+    size_t i,a;
+    char *dstr,*sstr;
+    dstr = (char *) dst;
+    sstr = (char *) src;
+    i =ft_strlen(dst);
+    a = 0;
+    while(sstr[a])
+    {
+        if(sstr[a] && i < dstsize)
+            dstr[i]=sstr[a];
+        else
+        {
+            dstr [i] = '\0';
+            return (i);
+        }
+        i++;
+        a++;       
+    }
+    return i;
+
+}
+int main ()
+{
+    char dst[20] = "helloewewgwgw";
+    char src[]  = "mmmmmmmmmmm";
+    printf("%lu\n",strlcat(dst,src,20));
+    printf("%s",dst);
+}
