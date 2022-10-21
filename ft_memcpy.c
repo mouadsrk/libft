bf70005290b613fca4ef8e45212f8d8573d4e6c3
Mouad Serroukh <mserrouk@e2r7p1.1337.ma>
2022-10-21 23:17:50 +0100

    work

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -4,23 +4,26 @@ void *ft_memcpy(void  *dst, const void  *src, size_t n)
 {
    
     size_t i;
-    char *d,*s;
-    d = (char*) dst;
-    s = (char *)src;
+    unsigned char *d,*s;
+    d = (unsigned char*) dst;
+    s = (unsigned char *)src;
     i = 0;
-    if (!src && !n)
-        return (NULL);
-    while(n--)
+   
+    if(s || d)
     {
-       d[i] = s[i];
-       i++;
-    }      
-    return (dst);
+        while( i < n )
+        {
+            d[i] = s[i];
+            i++;
+        }
+        return ((void*)d);
+    }
+    return (0);
 }
 // int main()
 // {
-    
-    
-    
-//     printf("%s",ft_memcpy(((void *)0), ((void *)0), 0));
+// char src[] = "test basic du memcpy !";
+// 	char buff1[22];
+// 	char buff2[22];
+//     printf("%s",ft_memcpy(buff2, src, 22));
 // }
--- ft_memmove.c
+++ ft_memmove.c
@@ -3,9 +3,35 @@
 // #include <string.h>
 // #include<stdio.h>
 
-   void *ft_memmove(void *dst, const void *src,size_t len)
+   void *ft_memmove(void *dst, const void *src, size_t len)
    {
     char *d;
     char *s;
-    
-   }
+    size_t i;
+ 
+    d = (char *) dst;
+    s = (char*) src ; 
+    i = 0;
+    if(d > s)
+    {
+     while(d[i])
+     i++;
+     
+
+     
+
+
+    }
+    else
+      s = ft_memcpy(s,d, len);  
+    return( (void*)dst);
+}
+// int main()
+// {
+//   char d[] = "mouad serrouk";
+//   char s[]  = ft_memcpy(s,d,11);
+//   printf("%s\n",s);
+//     memmove(d + 2 , d  , 11);
+//     printf("%s",d);
+
+// }
