a6e6d2df627497ba7648168740e9a71d99833a95
Mouad Serroukh <mserrouk@e1r8p1.1337.ma>
2022-10-22 22:38:15 +0100

    prt 1 fi

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,43 +1,5 @@
 #include"libft.h"
 
-size_t  ft_strlcpy(char * dst, const char * src, size_t dstsize)
-{
-    size_t a;
-
-    a= 0;
-    while(a < dstsize )
-    {
-        if(src[a])
-        {
-          if(a < dstsize -1)
-          {
-            dst[a] = src[a];
-            a++;
-          }  
-          else 
-          {
-            dst[a] = '\0';
-            return a = ft_strlen(src);
-          }
-        }
-        else
-        {
-            dst[a] = src[a];
-            return a = ft_strlen(src);
-        }
-    }
-   return a = ft_strlen(src);
-}
-// }
-
-// int main ()
-// {   
-//     char d[10];
-//     char s[0];
-//     printf("%lu\n",strlcpy(d,s,-1));
-//     printf("%s",d);
-// }
-
 // size_t  ft_strlcpy(char * dst, const char * src, size_t dstsize)
 // {
 //     size_t a;
@@ -60,9 +22,31 @@ size_t  ft_strlcpy(char * dst, const char * src, size_t dstsize)
 //         }
 //         else
 //         {
-//             dst[a] = src[a];
-//             return a = ft_strlen(sstr);
+//             dst[a] = '\0';
+//             return a = ft_strlen(src);
 //         }
 //     }
 //    return a = ft_strlen(src);
+// }
+size_t  ft_strlcpy(char * dst, const char * src, size_t dstsize)
+{
+    size_t a;
+
+    a= 0;
+    while(a < dstsize - 1 && src [a] && dstsize > 0)
+    {
+        dst[a] = src[a];
+        a++;
+    }
+    if(dstsize > 0)
+    dst[a] = '\0';
+    return a = strlen(src);
+}
+
+// int main()
+// {
+//     char *str = "BBBB";
+//     char buff1 [] = "helo";
+//     printf("%lu",ft_strlcpy(buff1,"a",0));
+//     printf("%s",buff1);
 // }
