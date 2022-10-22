14866a8dd0f818a449fc605eb433432e7d1cc2f1
Mouad Serroukh <mserrouk@e2r7p1.1337.ma>
2022-10-22 03:23:12 +0100

    w

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,14 +1,15 @@
 #include"libft.h"
+#include<limits.h>
 
 int ft_atoi(const char *str)
 {
     int i;
     int s;
-    int r;
+    long int r;
     
     i =0;
     r = 0;
-    s = 1;
+    s = 1;   
     while(str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
         i++;
     if (str[i] =='-' || str[i] == '+')
@@ -19,13 +20,18 @@ int ft_atoi(const char *str)
     }
         while(str[i] >= '0' && str[i] <= '9')
         {
+            if( r > 2147483647 && s == 1)
+                return -1;
+            if (r > 2147483648 && s == -1)
+                return 0;
             r = r * 10 + str[i] - 48;
             i++;
-        }
-    return r * s;            
+        }        
+    return ((int) r * s);            
 }
+
 // int main()
 // {
-//     char c[] = "1";
-//     printf("%d",ft_atoi(c));
+//     char n[40] = "99999999999999999999999999";
+//     printf("%d",atoi(n));
 // }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mserrouk <mserrouk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 17:49:41 by mserrouk          #+#    #+#             */
-/*   Updated: 2022/10/21 23:49:49 by mserrouk         ###   ########.fr       */
+/*   Updated: 2022/10/22 00:55:21 by mserrouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	j = 0;
 	i = 0;
 	ptr = (char *)haystack;
-	if (!haystack && !len)
-		return NULL;
-	if (needle[j] == '\0')
-		return (ptr);
+	if(!ptr  && len == 0)
+		return(NULL);
+	if(needle[j] == '\0')
+		return(ptr);
 	while (ptr[i] && needle[j] && i < len)
 	{
-		
 		while (ptr[i + j] == needle[j] && needle[j] && (i + j) < len)
 			j++;
 		if (j != strlen(needle) && j != 0)
@@ -38,3 +37,13 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		return (ptr + i - 1);
 	return (NULL);
 }
+
+
+// int main()
+// {
+// 	char em[] = "";
+// 	char s[] = "mouadf serrouk";
+// 	 printf("%s\n", ft_strnstr(((void *)0), "fake", 0));
+// 	 printf("%s\n",strnstr(((void *)0), "fake", 0));
+// 	printf("%s\n",em);	
+// }
