dc55c8724b14f11d95b0d96fb49ec50ee448347b
Mouad Serroukh <mserrouk@e1r8p1.1337.ma>
2022-10-23 01:42:15 +0100

    prt 1

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,7 +3,8 @@
 void *ft_memcpy(void  *dst, const void  *src, size_t n)
 {
     size_t i;
-    unsigned char *d,*s;
+    unsigned char *d;
+    unsigned char *s;
     d = (unsigned char*) dst;
     s = (unsigned char *)src;
     i = 0;
--- ft_memmove.c
+++ ft_memmove.c
@@ -1,37 +1,32 @@
 #include"libft.h"
 
-// #include <string.h>
-// #include<stdio.h>
 
-   void *ft_memmove(void *dst, const void *src, size_t len)
-   {
+
+ void *ft_memmove(void *dst, const void *src, size_t len)
+{
     char *d;
     char *s;
-    size_t i;
+    int a;
  
     d = (char *) dst;
     s = (char*) src ; 
-    i = 0;
+    a =  len  -1  ; 
     if(d > s)
     {
-     while(d[i])
-     i++;
-     
-
-     
-
-
+    while(a >= 0)
+    {
+      d[a] = s[a];
+     a--;
+    }
     }
     else
-      s = ft_memcpy(s,d, len);  
+      s = memcpy(s,d, len);  
     return( (void*)dst);
 }
 // int main()
 // {
 //   char d[] = "mouad serrouk";
-//   char s[]  = ft_memcpy(s,d,11);
-//   printf("%s\n",s);
-//     memmove(d + 2 , d  , 11);
+//     ft_memmove(d + 2 , d  , 11);
 //     printf("%s",d);
 
 // }
